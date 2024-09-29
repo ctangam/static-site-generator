60f32d6774c8694ebeafc6bdf58c4ef33a3e8f91
ctangam <luochenjoey@gmail.com>
2024-09-29 15:45:42 +0800

    add template

---
+++ readme.md
@@ -0,0 +1,3 @@
+Conceptually, a static site generator is straightforward.
+
+It takes some files as input, often markdown, render them, merge them with pre-defined templates, and output everything as raw HTML files. Simple, basic.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,66 @@
-fn main() {
-    println!("Hello, world!");
+use std::{fs, path::Path, thread, time::Duration};
+
+
+mod template;
+const CONTENT_DIR: &str = "content";
+const PUBLIC_DIR: &str = "public";
+
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    rebuild_site(CONTENT_DIR, PUBLIC_DIR).expect("Rebuilding site");
+
+    tokio::task::spawn_blocking(move || {
+        println!("listening for changes: {}", CONTENT_DIR);
+        let mut hotwatch = hotwatch::Hotwatch::new().expect("hotwatch failed to initialize!");
+        hotwatch
+            .watch(CONTENT_DIR, |_| {
+                println!("Rebuilding site");
+                rebuild_site(CONTENT_DIR, PUBLIC_DIR).expect("Rebuilding site");
+            })
+            .expect("hotwatch failed to watch content folder!");
+        loop {
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    Ok(())
+}
+
+fn rebuild_site(content_dir: &str, output_dir: &str) -> Result<(), anyhow::Error> {
+    let _ = fs::remove_dir_all(output_dir);
+
+    let markdown_files: Vec<String> = walkdir::WalkDir::new(content_dir)
+        .into_iter()
+        .filter_map(|e| e.ok())
+        .filter(|e| e.path().display().to_string().ends_with(".md"))
+        .map(|e| e.path().display().to_string())
+        .collect();
+    let mut html_files = Vec::with_capacity(markdown_files.len());
+
+    for file in &markdown_files {
+        let mut html = template::HEADER.to_owned();
+        let markdown = fs::read_to_string(file)?;
+        let parser = pulldown_cmark::Parser::new_ext(&markdown, pulldown_cmark::Options::all());
+
+        let mut body = String::new();
+        pulldown_cmark::html::push_html(&mut body, parser);
+
+        html.push_str(template::render_body(&body).as_str());
+        html.push_str(template::FOOTER);
+
+        let html_file = file.replace(content_dir, output_dir).replace(".md", ".html");
+        let folder = Path::new(&html_file).parent().unwrap();
+        let _ = fs::create_dir_all(folder);
+        fs::write(&html_file, html)?;
+
+        html_files.push(html_file);
+    }
+
+    write_index(html_files, output_dir)?;
+    Ok(())
+}
+
+fn write_index(files: Vec<String>, output_dir: &str) -> Result<(), anyhow::Error> {
+
+    Ok(())
 }
